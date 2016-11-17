02450d2e81ef7c83fa26d003cbbd9fae480fe958
amylmiller7 <amylmiller7@users.noreply.github.com>
2016-11-16 20:38:32 -0500

    Add files via upload
    Added files.

---
+++ CodingForWriters/CFW_3_nameInput/README_nameInput.md
@@ -0,0 +1,13 @@
+# Name Input
+
+Files:  
+* nameInput.html
+* nameInput.js
+
+Documentation:  
+* nameInput documentation.docx
+
+**Summary:** Manipulates the first name and last name that the user entered. Displays name on webpage two ways: first name followed by last name and last name followed by first name.   
+**JS:** Creates functions. Uses value property to retrieve values. Uses innerHTML to add results to webpage. 
+
+Date: September 2016
---
+++ CodingForWriters/CFW_3_nameInput/nameInput.js
@@ -0,0 +1,12 @@
+// Manipulates the first name and last name that the user entered. 
+// Displays name on webpage two ways: first name followed by last name and last name followed by first name. 
+
+//Concatenates the first name and last name that the user entered. Adds them to a webpage.  
+function getName() {
+    fullName.innerHTML = first.value + " " + last.value;
+}
+
+//Concatenates the last name and first name that the user entered. Adds them to a webpage. 
+function getNameReverse() {
+    lastNameFirst.innerHTML = last.value + " " + first.value;
+}
