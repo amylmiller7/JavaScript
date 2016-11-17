1a74ced1d8fde8e7a546331936593998433252aa
amylmiller7 <amylmiller7@users.noreply.github.com>
2016-11-16 20:38:07 -0500

    Add files via upload
    Added files.

---
+++ CodingForWriters/CFW_2_addAverageNumbers/README_addAverageNumbers.md
@@ -0,0 +1,13 @@
+# Add And Average Numbers
+
+Files:  
+* addAverageNumbers.html
+* addAverageNumbers.js
+
+Documentation:  
+* addAverageNumbers.docx
+
+**Summary:** Adds and averages numbers that the user entered. Converts text that the user entered from string to number. Changes width of text boxes to the number of characters that the user entered. Displays results on webpage.  
+**JS:**  Creates functions. Calls functions within other functions. Uses built-in function Number to convert text from string to number. Uses size property to set width of text box. Uses innerHTML to add results to webpage.
+
+Date: September 2016
---
+++ CodingForWriters/CFW_2_addAverageNumbers/addAverageNumbers.js
@@ -0,0 +1,33 @@
+// Adds and averages numbers that the user entered. 
+// Converts the text that the user entered from string to number. Changes width of text boxes to the number of characters that the user entered. Displays results on webpage.
+
+// Displays results of adding and averaging numbers that the user entered. Calls setWidth(), add(), and average(). Calls built-in function Number to convert text that user entered from string to number. 
+function displayResults() {
+    // Calls setWidth() function to set width of text boxes.
+    setWidth(num1, Number(num1.value));
+    setWidth(num2, Number(num2.value));
+    
+    // Calls add() function.
+    var addResult = add(Number(num1.value), Number(num2.value)),
+        // Calls average() function.
+        averageResult = average(Number(num1.value), Number(num2.value)); 
+            
+    // Adds addResult and averageResult to webpage.
+    resultAdd.innerHTML = "Total: " + addResult;
+    resultAverage.innerHTML = "Average: " + averageResult;
+}
+
+// Sets the width of a text box to the number of characters that equals the number entered in the text box.
+function setWidth(textBox, numChar) {
+    textBox.size = numChar;
+}
+
+// Returns the total of two numbers that the user entered.
+function add(x, y) {
+    return (x + y);
+} 
+
+// Returns the average of two numbers that the user entered. 
+function average(x, y) {
+    return (x + y) / 2;   
+}
