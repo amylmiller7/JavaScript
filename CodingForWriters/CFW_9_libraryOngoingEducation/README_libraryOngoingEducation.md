f05b083efc561570a711034e89d10d3060736f3b
amylmiller7 <amylmiller7@users.noreply.github.com>
2017-03-18 14:51:36 -0400

    Add files via upload

---
+++ CodingForWriters/CFW_9_libraryOngoingEducation/README_libraryOngoingEducation.md
@@ -0,0 +1,10 @@
+# Library Ongoing Education
+
+**Files:** 
+* libraryOngoingEducation.html
+* libraryOngoingEducation.js
+* awesomechart.js
+
+**Summary:** Displays select Ongoing Education coursework in chart form. Uses a JavaScript library to display the chart. Data gathered from list of recent coursework in XML repo, project Ongoing Education.
+
+Date: March 2017
---
+++ CodingForWriters/CFW_9_libraryOngoingEducation/libraryOngoingEducation.js
@@ -0,0 +1,13 @@
+// Displays select Ongoing Education coursework in chart form. Uses a JavaScript library to display the chart. 
+
+function drawChart() {
+	// !! converts JS object to true if not null, false if null
+	// Checks if canvas element is supported
+	if (!!document.createElement('canvas').getContext) {
+		var mychart = new AwesomeChart('canvas1');
+		mychart.data = [4, 1, 4, 2, 5, 5, 1, 1, 10, 1, 2, 3];
+		mychart.title = "Ongoing Education Coursework \u2014 Select Topics";
+		mychart.labels = ["API", "Command Line", "CSS", "Git/GitHub", "HTML", "JavaScript", "RoboHelp", "Tech Editing", "Tech Writing", "Web Design", "Web Dev", "XML"];
+		mychart.draw();
+	}
+}
